11e8a79c14cfffc0aeea8cc0a95a6e1fce867746
waiyanyenaing <wai.yan.ye.naing@fusionsol.com>
2022-07-05 13:57:31 +0630

    up

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import firebase from 'firebase/compat/app'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { IoIosSend } from 'react-icons/io'
 
 firebase.initializeApp({
   apiKey: 'AIzaSyCWr4slB3wOzFn8VEyDUUEF4xQS0joDepY',
@@ -21,32 +22,36 @@ const ChatRoom = () => {
   console.log(messages)
 
   return (
-    // <div className="grid grid-cols-3">
-    //   <div>left</div>
-    //   <div className="col-span-2 bg-white rounded-xl">
-    //     <div></div>
-    //   </div>
-    // </div>
-    <div>
-      {messages &&
-        messages.map((message, id) => (
-          <ChatMessage key={id} message={message} />
-        ))}
-      <InputMessage />
+    <div className="grid grid-cols-3 h-[85vh]">
+      <div>left</div>
+      <div className="text-black col-span-2 bg-white rounded-2xl px-8 py-5 flex flex-col justify-between">
+        <div className="overflow-auto h-[83vh] pb-8">
+          {messages &&
+            messages.map((message, id) => (
+              <ChatMessage key={id} message={message} />
+            ))}
+        </div>
+        <InputMessage />
+      </div>
     </div>
   )
 }
 
 const ChatMessage = ({ message }) => {
   return (
-    <div>
-      <div>
-        <img src={message.photoURL} />
+    <div className="flex mt-3">
+      <div className="w-10 h-10 mr-5">
+        <img className="rounded-full" src={message.photoURL} />
+        <div className="text-xs mt-1">
+          {message.createdAt != null &&
+            message.createdAt.toDate().toLocaleTimeString('en-GB', {
+              hour: '2-digit',
+              minute: '2-digit',
+            })}
+        </div>
       </div>
-      <div>{message.text}</div>
-      <div>
-        {message.createdAt != null &&
-          message.createdAt.toDate().toLocaleString()}
+      <div className="max-w-[58%] text-left px-5 py-5 bg-bgSecondary rounded-b-3xl rounded-tr-2xl shadow-md">
+        <p className="text-sm">{message.text}</p>
       </div>
     </div>
   )
@@ -59,25 +64,48 @@ const InputMessage = () => {
 
   const sendMessage = async (e) => {
     e.preventDefault()
+    if (formValue !== '') {
+      const { uid, photoURL } = auth.currentUser
+      console.log(uid)
 
-    const { uid, photoURL } = auth.currentUser
-    console.log(uid)
+      await messagesRef.add({
+        text: formValue,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        photoURL,
+      })
 
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL,
-    })
-
-    setFormValue('')
+      setFormValue('')
+    }
   }
 
   return (
-    <form onSubmit={sendMessage}>
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
-      <button type="submit">Send</button>
-    </form>
+    // <form onSubmit={sendMessage}>
+    //   <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+    //   <button type="submit">Send</button>
+    // </form>
+    <div className="">
+      <form className="w-full flex" onSubmit={sendMessage}>
+        <div className="w-[90%] mr-3">
+          <input
+            className="bg-bgSecondary text-sm appearance-none rounded-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none focus:bg-gray-200 focus:border-purple"
+            id="inline-full-name"
+            type="text"
+            placeholder="Type a message..."
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
+          />
+        </div>
+        <div className="">
+          <button
+            type="submit"
+            className="bg-green text-white w-11 h-11 flex justify-center items-center rounded-full"
+          >
+            <IoIosSend size={'23px'} />
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
