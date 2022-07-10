243e449c02618dc9e4cf7c3ca2edefbcb3e13ce6
waiyanyenaing <wai.yan.ye.naing@fusionsol.com>
2022-07-10 16:31:49 +0630

    ok

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useState } from 'react'
 import { IoIosLogOut } from 'react-icons/io'
 import { auth } from '../firebase'
 const NavBar = () => {
+  const [nothing, setNothing] = useState(false)
+
   return (
     <div>
       <nav className="flex items-center justify-between flex-wrap bg-[#F5F7FB] p-6">
@@ -58,37 +61,95 @@ const NavBar = () => {
           <div className="text-md font-bold lg:flex-grow">
             <a
               href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-[#636365] mr-10"
+              className="block mt-4 lg:inline-block lg:mt-0 text-[#636365] mr-10 hover:scale-105 ease-in duration-100 transition"
+              onClick={() => setNothing(true)}
             >
               Home
             </a>
+
+            <div
+              id="defaultModal"
+              tabindex="-1"
+              aria-hidden="true"
+              class={`"overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full" ${
+                nothing ? 'block' : 'hidden'
+              }`}
+            >
+              <div
+                class="relative p-4 w-full h-full flex justify-center items-center bg-black bg-opacity-60"
+                onClick={() => setNothing(false)}
+              >
+                <div class="relative bg-white rounded-lg shadow text-black max-w-[500px] px-6 py-10">
+                  <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                    <h3 class="text-xl font-bold tracking-tighter">
+                      Dear User 🤖
+                    </h3>
+                    <img
+                      src="https://img.icons8.com/external-flaticons-flat-flat-icons/250/000000/external-system-e-commerce-flaticons-flat-flat-icons.png"
+                      alt="logo"
+                      className="w-36 h-36 absolute right-20 bottom-[9rem]"
+                    />
+                    <button
+                      type="button"
+                      class="text-gray-400 bg-transparent hover:bg-gray-200 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                      data-modal-toggle="defaultModal"
+                      onClick={() => setNothing(false)}
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                  <div class="p-6 space-y-6">
+                    <p class="text-sm font-semibold">
+                      Sorry! This function is not yet available. Please check
+                      back later. Thank you for your patience.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
             <a
               href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 border-b-2 border-green text-green mr-10"
+              className="block mt-4 lg:inline-block lg:mt-0 border-b-2 border-green text-green mr-10 hover:scale-105 ease-in duration-100 transition"
+              onClick={() => setNothing(true)}
             >
               Chat
             </a>
             <a
               href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-[#636365] mr-10"
+              className="block mt-4 lg:inline-block lg:mt-0 text-[#636365] mr-10 hover:scale-105 ease-in duration-100 transition"
+              onClick={() => setNothing(true)}
             >
               Contacts
             </a>
             <a
               href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-[#636365] mr-10"
+              className="block mt-4 lg:inline-block lg:mt-0 text-[#636365] mr-10 hover:scale-105 ease-in duration-100 transition"
+              onClick={() => setNothing(true)}
             >
               Setting
             </a>
             <a
               href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-[#636365] mr-10"
+              className="block mt-4 lg:inline-block lg:mt-0 text-[#636365] mr-10 hover:scale-105 ease-in duration-100 transition"
+              onClick={() => setNothing(true)}
             >
               FAQs
             </a>
             <a
               href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-[#636365] mr-20"
+              className="block mt-4 lg:inline-block lg:mt-0 text-[#636365] mr-20 hover:scale-105 ease-in duration-100 transition"
+              onClick={() => setNothing(true)}
             >
               Terms of use
             </a>
